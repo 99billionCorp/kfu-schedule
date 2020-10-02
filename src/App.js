bf354d98258c90fdcd137083a0caff0ee65310f8
artif1c1al <zhuravlenkoartem@gmail.com>
2020-10-02 22:53:21 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -5,29 +5,27 @@ import Prepod from "./images/muranov.jpg";
 function App() {
   return (
     <div className="App">
-      <div className="App__wrapper">
-        <div className="card card-fullScreen">
-          <form className="filter">
-            <p className="filter__header">Где ты учишься?</p>
-            <input
-              className="input-form filter__input"
-              type="text"
-              value="Математика и информатика"
-            />
-            <input
-              className="input-form filter__input"
-              type="text"
-              placeholder="Группа..."
-            />
-            <input
-              className="btn btn-dark btn-large filter__submit"
-              type="submit"
-              value="Сохранить"
-            />
-          </form>
+      <div className="card card-fullScreen">
+        <form className="filter">
+          <p className="filter__header">Где ты учишься?</p>
+          <input
+            className="input-form filter__input"
+            type="text"
+            value="Математика и информатика"
+          />
+          <input
+            className="input-form filter__input"
+            type="text"
+            placeholder="Группа..."
+          />
+          <input
+            className="btn btn-dark btn-large filter__submit"
+            type="submit"
+            value="Сохранить"
+          />
+        </form>
 
-          <span class="material-icons icon-info">info</span>
-        </div>
+        <span class="material-icons icon-info">info</span>
       </div>
     </div>
   );
