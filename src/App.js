34a1056c301a7e13fb14338a003988a451e0b600
artif1c1al <zhuravlenkoartem@gmail.com>
2020-10-02 22:19:19 +0300

    new features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./scss/index.scss";
+import Prepod from "./images/muranov.jpg";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <div className="App__wrapper">
+        <div className="card card-fullScreen">
+          <form className="filter">
+            <p className="filter__header">Где ты учишься?</p>
+            <input
+              className="input-form filter__input"
+              type="text"
+              value="Математика и информатика"
+            />
+            <input
+              className="input-form filter__input"
+              type="text"
+              placeholder="Группа..."
+            />
+            <input
+              className="btn btn-dark btn-large filter__submit"
+              type="submit"
+              value="Сохранить"
+            />
+          </form>
+
+          <span class="material-icons icon-info">info</span>
+        </div>
+      </div>
+    </div>
   );
 }
 
