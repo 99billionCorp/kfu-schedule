5fad920000604707deea6e03a8809d5f81d113c6
artif1c1al <zhuravlenkoartem@gmail.com>
2020-10-05 21:56:25 +0300

    finish branch

--- client/src/components/card_schedule/lesson/index.js
+++ client/src/components/card_schedule/lesson/index.js
@@ -1,16 +1,13 @@
-import React from "react"
-import './index.scss'
-export default function lesson({lessonNum, lessonName, teacher, classroom}){
-    return (
-        <div className="lesson">
-            <div className="lesson__circle">{lessonNum}</div>
-            <div className="lesson__right">
-                <div className="lesson__info">
-                    <div className="lesson__name">{lessonName}</div>
-                    <div className="lesson__teacher">{teacher}</div>
-                </div>
-                <div className="lesson__classroom">{classroom}</div>
-            </div>
-        </div>
-    )
+import React from "react";
+import "./index.scss";
+export default function lesson({ lessonNum, lessonName, teacher, classroom }) {
+  return (
+    <div className="lesson">
+      <div className="lesson__info">
+        <div className="lesson__name ">{lessonName}</div>
+        <div className="lesson__teacher">{teacher}</div>
+      </div>
+      <div className="lesson__classroom">{classroom}</div>
+    </div>
+  );
 }
