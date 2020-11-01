4dec64c2f18ef49451b92932135c94386f1c09d3
mebelh <memetbelualov@bk.ru>
2020-11-01 14:22:06 +0300

    Build

---
+++ client/build/precache-manifest.53562b9e2263b2d00668958ede272a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d03a6bd96845037136f8cebb0b568a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "014fd000e09c08aab82c",
+    "url": "/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "ecc35e072ab6add235fe",
+    "url": "/static/css/main.617f1d60.chunk.css"
+  },
+  {
+    "revision": "014fd000e09c08aab82c",
+    "url": "/static/js/2.e9e9d945.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.e9e9d945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc35e072ab6add235fe",
+    "url": "/static/js/main.46791eb4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
